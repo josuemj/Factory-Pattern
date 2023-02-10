a95f112c6583c8e7a661e664bfbd563f09a3e0ce
MarchMol <98711734+MarchMol@users.noreply.github.com>
2023-02-10 12:15:29 -0600

    Computer facrory
    :D

--- src/FactoryComputer.java
+++ src/FactoryComputer.java
@@ -1,2 +1,12 @@
 public class FactoryComputer {
+    public Computer getComputer(String type){
+        switch(type){
+            case "HpComputer":
+                return new HpComputer();
+            case "MacComputer":
+                return new MacComputer();
+            default: // Dell
+                return new DellComputer();
+        }
+    }
 }
